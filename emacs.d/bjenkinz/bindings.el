e8a0bcc0c549e8d461136143d7af50f148dca7f7
Brian Jenkins <bj@jenkinz.com>
2012-05-19 19:35:40 -0700

    Added C-z undo key binding; disabled keyboard macros

--- emacs.d/bjenkinz/bindings.el
+++ emacs.d/bjenkinz/bindings.el
@@ -1,4 +1,4 @@
-;;; Global key bindigns
+;;; Global key bindings
 
 ;; How to Define Keyboard Shortcuts in Emacs
 ;; http://xahlee.org/emacs/keyboard_shortcuts.html
@@ -10,12 +10,12 @@
 (global-set-key [(control kp-2)] 'shrink-window)
 
 ;; Keyboard macros
-(global-set-key [(shift f4)] 'kmacro-start-macro-or-insert-counter)
+;; (global-set-key [(shift f4)] 'kmacro-start-macro-or-insert-counter)
 ;; (global-set-key [(f4)]    'kmacro-end-or-call-macro)  ;; already defined
 
 ;; Refresh-like
-;; (global-set-key [(f5)]         'revert-buffer)
-;; (global-set-key [(control f5)] 'revbufs)
+(global-set-key [(f5)]         'revert-buffer)
+(global-set-key [(control f5)] 'revbufs)
 
 ;; Indenting and alignment
 ;; (global-set-key [(f8)]         'indent-region)
@@ -24,8 +24,8 @@
 ;; (global-set-key [(meta f8)]    'align-regexp)
 
 ;; Version control and change related
-;(global-set-key [(control f9)]      'rails-svn-status-into-root)  ;; Move to rails mode?
-;(global-set-key [(control meta f9)] (lambda () (interactive) (svn-status default-directory)))
+;; (global-set-key [(control f9)]      'rails-svn-status-into-root)  ;; Move to rails mode?
+;; (global-set-key [(control meta f9)] (lambda () (interactive) (svn-status default-directory)))
 (global-set-key [(control f9)] (lambda () (interactive) (magit-status default-directory)))
 (global-set-key [(f9)]         (lambda () (interactive) (magit-status default-directory)))
 (global-set-key [(meta f9)]    'autotest-switch)  ;; Move to ruby/rails mode?
@@ -36,3 +36,6 @@
 ;; Easier buffer killing
 (global-unset-key (kbd "M-k"))
 (global-set-key (kbd "M-K") 'kill-this-buffer)
+
+;; Easier Undo
+(global-set-key (kbd "C-z") 'undo)
