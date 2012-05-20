f00c6d2a370cb671148a9551993abaa1551f6468
Brian Jenkins <bj@jenkinz.com>
2012-05-19 19:50:00 -0700

    Removed comments from mac.el

--- emacs.d/bjenkinz/mac.el
+++ emacs.d/bjenkinz/mac.el
@@ -7,13 +7,3 @@
 ;; Slow down the mouse wheel acceleration
 (when (boundp 'mouse-wheel-scroll-amount)
   (setq mouse-wheel-scroll-amount '(0.01)))
-
-;; Prevent anti aliasing
-;; (setq mac-allow-anti-aliasing nil)
-
-;; Mac-style key-binding mode.  Aquamacs-like
-;; (mac-key-mode)
-
-;; Full screen mode
-;; (mac-hide-menu-bar)
-;; (mac-show-menu-bar)
