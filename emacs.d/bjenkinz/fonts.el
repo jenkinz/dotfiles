14c2048939836629c0036994b089c71637028c08
Brian Jenkins <bj@jenkinz.com>
2013-02-10 19:02:27 -0800

    Changed default emacs font to Menlo-16.
    Followed instructions from
    http://christianromney.com/post/11597491568/set-a-default-emacs-font-the-easy-way-on-mac-os-x,
    very helpful for a novice!

--- emacs.d/bjenkinz/fonts.el
+++ emacs.d/bjenkinz/fonts.el
@@ -1,12 +1,14 @@
 ;;; Fonts
 
+(set-default-font "-apple-Menlo-medium-normal-normal-*-15-*-*-*-m-0-iso10646-")
+
 ;; From: http://community.schemewiki.org/cgi-bin/scheme.cgi?Emacs
-(and window-system
-     (create-fontset-from-fontset-spec
-      (concat
-       "-apple-monaco-medium-r-normal--12-*-*-*-*-*-fontset-monaco,"
-       "ascii:-apple-monaco-medium-r-normal--12-100-*-*-m-100-mac-roman"))
-     )
+;;(and window-system
+;;     (create-fontset-from-fontset-spec
+;;      (concat
+;;       "-apple-monaco-medium-r-normal--12-*-*-*-*-*-fontset-monaco,"
+;;       "ascii:-apple-monaco-medium-r-normal--12-100-*-*-m-100-mac-roman"))
+;;     )
 
 ;; To change a font size globally:
 ;; (set-face-attribute 'default nil :height 360)
