63d71dd1e156fc8cf0702ae50c8a85496c72dc2c
Brian Jenkins <bj@jenkinz.com>
2013-04-27 11:38:58 -0700

    Delete old emacs configuration - part 1

--- emacs.d/bjenkinz/mac.el
+++
@@ -1,9 +0,0 @@
-;;; Cocoa!
-
-;; Hide the tool bar
-(when (fboundp 'tool-bar-mode)
-  (tool-bar-mode 0))
-
-;; Slow down the mouse wheel acceleration
-(when (boundp 'mouse-wheel-scroll-amount)
-  (setq mouse-wheel-scroll-amount '(0.01)))
--- emacs.d/bjenkinz/scala.el
+++
@@ -1,8 +0,0 @@
-;; Scala mode
-(add-to-list 'load-path "~/.emacs.d/vendor/scala")
-(require 'scala-mode-auto)
-
-;; Ensime, for play framework (http://playframework.org)
-(add-to-list 'load-path "~/.emacs.d/vendor/ensime/elisp")
-(require 'ensime)
-(add-hook 'scala-mode-hook 'ensime-scala-mode-hook) ; starts ensime when scala mode is started
