3e316409b85a8696ba35b971ce160f372b6626a5
Brian Jenkins <bj@jenkinz.com>
2013-07-18 06:50:09 -0700

    El-get update

--- macosx/emacs.d/el-get/.status.el
+++ macosx/emacs.d/el-get/.status.el
@@ -1,21 +1,21 @@
-((auto-complete status "installed" recipe
-                (:name auto-complete :website "https://github.com/auto-complete/auto-complete" :description "The most intelligent auto-completion extension." :type github :pkgname "auto-complete/auto-complete" :depends
-                       (popup fuzzy)))
- (ctable status "installed" recipe
-         (:name ctable :description "Table Component for elisp" :type github :pkgname "kiwanami/emacs-ctable"))
- (deferred status "installed" recipe
+((auto-complete status "required" recipe
+		(:name auto-complete :website "https://github.com/auto-complete/auto-complete" :description "The most intelligent auto-completion extension." :type github :pkgname "auto-complete/auto-complete" :depends
+		       (popup fuzzy)))
+ (ctable status "required" recipe
+	 (:name ctable :description "Table Component for elisp" :type github :pkgname "kiwanami/emacs-ctable"))
+ (deferred status "required" recipe
    (:name deferred :description "Simple asynchronous functions for emacs lisp" :website "https://github.com/kiwanami/emacs-deferred" :type github :pkgname "kiwanami/emacs-deferred" :features "deferred"))
  (el-get status "installed" recipe
-         (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "4.stable" :pkgname "dimitri/el-get" :info "." :load "el-get.el"))
- (epc status "installed" recipe
+	 (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "4.stable" :pkgname "dimitri/el-get" :info "." :load "el-get.el"))
+ (epc status "required" recipe
       (:name epc :description "An RPC stack for Emacs Lisp" :type github :pkgname "kiwanami/emacs-epc" :depends
-             (deferred ctable)))
- (fuzzy status "installed" recipe
-        (:name fuzzy :website "https://github.com/auto-complete/fuzzy-el" :description "Fuzzy matching utilities for GNU Emacs" :type github :pkgname "auto-complete/fuzzy-el"))
- (jedi status "installed" recipe
+	     (deferred ctable)))
+ (fuzzy status "required" recipe
+	(:name fuzzy :website "https://github.com/auto-complete/fuzzy-el" :description "Fuzzy matching utilities for GNU Emacs" :type github :pkgname "auto-complete/fuzzy-el"))
+ (jedi status "required" recipe
        (:name jedi :description "An awesome Python auto-completion for Emacs" :type github :pkgname "tkf/emacs-jedi" :build
-              (("make" "requirements"))
-              :submodule nil :depends
-              (epc auto-complete)))
- (popup status "installed" recipe
-        (:name popup :website "https://github.com/auto-complete/popup-el" :description "Visual Popup Interface Library for Emacs" :type github :pkgname "auto-complete/popup-el")))
+	      (("make" "requirements"))
+	      :submodule nil :depends
+	      (epc auto-complete)))
+ (popup status "required" recipe
+	(:name popup :website "https://github.com/auto-complete/popup-el" :description "Visual Popup Interface Library for Emacs" :type github :pkgname "auto-complete/popup-el")))
